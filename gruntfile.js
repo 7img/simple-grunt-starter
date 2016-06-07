1e2c935ca6574af89c16f70a1bdfbf13b685c4c1
Tim Geerlings <Tim@pangaea.nl>
2016-06-07 17:40:00 +0200

    Init

---
+++ gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+	'use strict';
+
+	// Load all grunt packages
+	require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+						'dist/css/style.css': [
+							'src/scss/style.scss',
+							'src/scss/*/*.scss',
+							'src/scss/*/*/*.scss',
+							'src/scss/*/*/*/*.scss'
+						]
+				}
+			}
+		},
+		uglify: {
+				options: {
+					banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd") %> */ \n'
+				},
+			targets: {
+				files: {
+					'dist/js/custom.min.js' : 'dist/js/custom.js',
+				} //Files
+			}
+		},
+		concat: {
+				options: {
+					banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+						'<%= grunt.template.today("yyyy-mm-dd") %> */ \n'
+				},
+			targets: {
+				files: {
+					'dist/js/custom.js' : ['src/js/*/*/*.js', 'src/js/*/*.js', 'src/js/custom.js'],
+				} //Files
+			}
+		}, //concat
+		cssmin: {
+			files: {
+					expand: true,
+					cwd: 'dist/css/',
+					src: ['*.css', '!*.min.css'], // 1
+					dest: 'dist/css/',
+					ext: '.min.css'
+			}
+		},
+		copy_mate: {
+			boostrap_fonts : {
+				options: {
+					type: "single"
+				},
+				src: 'node_modules/bootstrap-sass/assets/fonts/',
+				destDir: 'dist/fonts'
+			},
+			bootstrap_images : {
+				options: {
+					type: "single"
+				},
+				src: 'node_modules/bootstrap-sass/assets/images/',
+				destDir: 'dist/images'
+			}
+	  },
+		watch : {
+			scripts: {
+			    files: [
+              'src/js/*.js',
+              'src/js/*/*.js',
+              'src/js/*/*/*.js'
+          ],
+			    tasks: ['concat']
+				}, //scripts
+				sass: {
+				    files: [
+	              'src/scss/*.scss',
+				        'src/scss/*/*.scss',
+				        'src/scss/*/*/*.scss'
+				    ],
+				   	tasks: ['sass']
+				}
+		}
+	}); //initConfig
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('init', ['copy_mate', 'concat', 'uglify', 'sass', 'cssmin']);
+	grunt.registerTask('build', ['concat', 'uglify', 'sass', 'cssmin']);
+} //exports
---
+++ readme.md
@@ -0,0 +1,50 @@
+# Welcome to simple-grunt-starter
+
+This is a Grunt starter kit with Bootstrap that I use for my own projects.
+
+## Simple Grunt starter:
+- Compiles SCSS / SASS to one style.css file.
+- Also minifies that style.css to style.min.css. For use on your live websites.
+- Automaticly includes bootstrap-sass that you can overwrite with your own variables. Theming your Bootstrap has never been easier!
+- Add any scss file and it gets included automaticly!
+- Checks your js files for errors.
+- Combines JS files to one custom.js file.
+- Adds Bootstrap javascript to your javascript file.
+- Minifies that javascript file to a custom.min.js for your live websites.
+- Add any javascript file and it gets included automaticly!
+
+## Getting Started
+
+Install [Node.js](https://nodejs.org/en/) and [Grunt](http://gruntjs.com/).
+
+For more information on Grunt, see the [Getting Started](http://gruntjs.com/getting-started) guide.
+
+If Node.js and Grunt are installed navigate to the destination folder using the Node.js command prompt.
+
+For example:
+```
+cd C:\Development\GIT\simple-grunt-starter
+```
+
+To get started we will need to install the required packages specified in package.json. This is done by entering the following command in your Node.js command prompt:
+```
+npm install
+```
+
+Now that the required packages are installed run the following command to create the folders and copy the required fonts to the dist folder:
+```
+grunt init
+```
+
+Now run:
+```
+grunt
+```
+
+Done! Grunt is now watching for your changes and writes them to the  dist folder! 
+
+Ready to go live? Run the following command to minify your files:
+```
+grunt
+```
+Don't forget to change that paths in your html to the .min version!
