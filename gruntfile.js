6f8e14b4099d1ad39f4f05a259b5544e88c2512c
Tim Geerlings <Tim@pangaea.nl>
2016-06-09 14:22:33 +0200

    Recursive issue

--- gruntfile.js
+++ gruntfile.js
@@ -70,11 +70,7 @@ module.exports = function(grunt) {
 	  },
 		watch : {
 			scripts: {
-			    files: [
-              'src/js/*.js',
-              'src/js/*/*.js',
-              'src/js/*/*/*.js'
-          ],
+			    files: ['src/js/**/*.js'],
 			    tasks: ['concat']
 				}, //scripts
 				sass: {
